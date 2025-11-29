1d0dfc67448ecee55648d965261e0d56ae5007ca
M∎r <140930+codekiln@users.noreply.github.com>
2025-11-29 23:52:25 +0000

    Reorganize main function to enable print_labeled_measurement and clarify control flow with try_1 and try_2

--- exercises/05_functions/src/main.rs
+++ exercises/05_functions/src/main.rs
@@ -1,6 +1,16 @@
 fn main() {
-    // print_labeled_measurement(5, 'h');
-    f(0);
+    print_labeled_measurement(5, 'h');
+    // f(0);
+    for cond in [true, false] {
+        println!("cond = {cond}");
+        try_1(cond);
+        try_2(cond);
+    }
+    // just checking ... 
+    let x;
+    let cond: bool = false;
+    if cond { x = 5; } else { x = 6; }
+    println!("{x}"); // 6
 }
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
@@ -12,3 +22,17 @@ fn print_labeled_measurement(value: i32, unit_label: char) {
 //     println!("{x}");
 // }
 
+fn try_1(cond: bool) {
+    let x = if cond { 1 } else { 2 };
+    println!("{x}");
+}
+
+fn try_2(cond: bool) {
+    let x;
+    if cond {
+        x = 1;
+    } else {
+        x = 2;
+    }
+    println!("{x}");
+}
